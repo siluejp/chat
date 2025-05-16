f648b752bfccf049b07f6edb4c9ef34beb3e9139
suzuki shinji <suzukishinji@suzukinoMacBook-Air.local>
2025-05-17 00:10:15 +0900

    embedding

--- llm.py
+++ llm.py
@@ -6,6 +6,7 @@ from simple_tokenizer_v2 import SimpleTokenizerV2
 from gpt_dataset_v1 import GPT_Dataset_V1
 from importlib.metadata import version
 import tiktoken
+import torch
 from torch.utils.data import DataLoader
 
 if not os.path.exists("the-verdict.txt"):
@@ -143,3 +144,38 @@ data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
 print("inputs:\n", inputs)
 print("targets:\n", targets)
+
+print("単語の位置をエンコードする")
+
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size,output_dim)
+
+print(token_embedding_layer)
+
+max_length = 4
+
+dataloader = create_dataloader_v1(
+    raw_text,
+    batch_size=8,
+    max_length=max_length,
+    stride=max_length,
+    shuffle=False
+)
+
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+
+print("Token IDs:\n", inputs)
+print("\nInputs shape：\n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+print(token_embeddings.shape)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print(pos_embeddings.shape)
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
