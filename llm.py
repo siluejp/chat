3ea04a8a26d1e54e3d4397f690909ce8c94f28f8
suzuki shinji <suzukishinji@suzukinoMacBook-Air.local>
2025-05-16 23:09:44 +0900

    add DataLoader

--- llm.py
+++ llm.py
@@ -3,8 +3,10 @@ import re
 import urllib.request
 from simple_tokenizer_v1 import SimpleTokenizerV1
 from simple_tokenizer_v2 import SimpleTokenizerV2
+from gpt_dataset_v1 import GPT_Dataset_V1
 from importlib.metadata import version
 import tiktoken
+from torch.utils.data import DataLoader
 
 if not os.path.exists("the-verdict.txt"):
     url = ("https://raw.githubusercontent.com/rasbt/"
@@ -95,3 +97,49 @@ for i in range(1, context_size+1):
     context = enc_sample[:i]
     desired = enc_sample[i]
     print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+        tokenizer = tiktoken.get_encoding("gpt2")
+
+        dataset = GPT_Dataset_V1(txt, tokenizer, max_length, stride)
+
+        dateloader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last,
+            num_workers=num_workers
+        )
+
+        return dateloader
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text,
+    batch_size=1,
+    max_length=4,
+    stride=1,
+    shuffle=False
+)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+
+second_batch = next(data_iter)
+print(second_batch)
+
+print("バッチサイズ変更")
+dataloader = create_dataloader_v1(
+    raw_text,
+    batch_size=8,
+    max_length=4,
+    stride=4,
+    shuffle=False
+)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("inputs:\n", inputs)
+print("targets:\n", targets)
